b42605079a62b7f749067099828a6a5f05458dcb
Simka-for <58855519+simka-for@users.noreply.github.com>
2020-04-18 15:20:07 +0300

    Multithreaded generation established

--- src/Generator.java
+++ src/Generator.java
@@ -1,45 +1,36 @@
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-
 public class Generator implements Runnable{
 
     private final int regionCode;
 
-    Generator(int regionCode) throws FileNotFoundException {
+    Generator(int regionCode) {
         this.regionCode = regionCode;
     }
-
-    private final PrintWriter writer = new PrintWriter("res/num_multi.txt");
 
 
     @Override
     public void run() {
 
-        long start = System.currentTimeMillis();
-
         char[] letters = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
-            for (int number = 1; number < 1000; number++) {
-                for (char firstLetter : letters) {
-                    for (char secondLetter : letters) {
-                        for (char thirdLetter : letters) {
-                            StringBuilder builder = new StringBuilder();
-                            builder.append(firstLetter);
-                            builder.append(padNumber(number, 3));
-                            builder.append(secondLetter);
-                            builder.append(thirdLetter);
-                            builder.append(padNumber(regionCode, 2));
-                            builder.append("\n");
-                            writer.write(builder.toString());
-                        }
+
+        StringBuilder carNumber = new StringBuilder();
+        for (char firstLetter : letters) {
+            for (char secondLetter : letters) {
+                for (char thirdLetter : letters) {
+                    for (int number = 1; number < 1000; number++) {
+                        carNumber.append(firstLetter)
+                                .append(padNumber(number, 3))
+                                .append(secondLetter).append(thirdLetter)
+                                .append(padNumber(regionCode, 2))
+                                .append("\n");
                     }
                 }
             }
-        writer.flush();
-        writer.close();
-        System.out.println("Регион №" + getRegionCode()+ " сгенерирован. " + (System.currentTimeMillis() - start) + " ms");
+        }
+        Main.queue.add(carNumber.toString());
     }
 
     private static String padNumber(int number, int numberLength) {
+
         StringBuilder numberStr = new StringBuilder(Integer.toString(number));
         int padSize = numberLength - numberStr.length();
         for (int i = 0; i < padSize; i++) {
@@ -48,7 +39,4 @@ public class Generator implements Runnable{
         return numberStr.toString();
     }
 
-    public int getRegionCode() {
-        return regionCode;
-    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,49 @@
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
 
-    private static final int processorCoreCount = Runtime.getRuntime().availableProcessors();
-    private static final int regionsCount = 2;
+    protected static final Queue<String> queue = new ConcurrentLinkedQueue<>();
+    private static final AtomicInteger counter = new AtomicInteger(0);
+    private static final int processorCoreCount = (Runtime.getRuntime().availableProcessors());
+    private static final Path targetPath = Paths.get("res/numbers.txt");
 
-    public static void main(String[] args) throws FileNotFoundException, InterruptedException {
+    private static final int regionCount = 3;
+
+
+    public static void main(String[] args) {
 
         long startTime = System.currentTimeMillis();
+        String poll;
 
         ExecutorService executorService = Executors.newFixedThreadPool(processorCoreCount);
 
-        for (int i = 1; i < regionsCount; i++) {
+        try {
+            for (int i = 1; i <= regionCount; i++)
 
-            executorService.submit(new Generator(i));
+                executorService.submit(new Generator(i));
+
+            while (counter.get() < regionCount - 1 || queue.size() > 0) {
+
+                if ((poll = queue.poll()) != null)
+                {
+                    System.out.printf("Регион № %.0f сгенерирован %n", (double) counter.incrementAndGet() / 99 * 100);
+                    Files.write(targetPath, poll.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+                }
+            }
             executorService.shutdown();
-            executorService.awaitTermination(1, TimeUnit.HOURS);
+            System.out.println((System.currentTimeMillis() - startTime) + " ms\n");
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
-        System.out.println((System.currentTimeMillis() - startTime) + " ms");
     }
 }
